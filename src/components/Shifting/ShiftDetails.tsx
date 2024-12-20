1588cff0f1541d25365ee613c709c8ecd8ab9de8
Raj kumar <150310085+rajku-dev@users.noreply.github.com>
2024-12-20 09:55:45 +0530

    Update src/components/Shifting/ShiftDetails.tsx
    Co-authored-by: Rithvik Nishad <rithvikn2001@gmail.com>

--- src/components/Shifting/ShiftDetails.tsx
+++ src/components/Shifting/ShiftDetails.tsx
@@ -43,7 +43,6 @@ export default function ShiftDetails(props: { id: string }) {
     return (
       <CopyButton
         content={copyContent(data)}
-        tooltipContent="Copy Details to Clipboard"
         size="sm"
       />
     );
