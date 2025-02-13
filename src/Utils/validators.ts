f3b1898b2a465cb8c3adf92b562e1279a2a84549
rajku-dev <totoregex@gmail.com>
2025-02-13 09:04:39 +0530

    reuse phone number validation translation

--- src/Utils/validators.ts
+++ src/Utils/validators.ts
@@ -13,7 +13,7 @@ export default () => ({
 
     required: z
       .string()
-      .min(1, { message: t("phone_number_is_required") })
+      .min(1, { message: t("phone_number_must_be_10_digits") })
       .refine((val) => isValidPhoneNumber(val), {
         message: t("phone_number_validation_error"),
       }),
